394ab1e394c022da0743c2e8f1c76b6918fde9fb
saritaborad <borad.sarita@gmail.com>
2023-12-01 16:00:40 +0530

    deploy script changes

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -13,27 +13,29 @@ uploads_folder="$deploy_path/backend/uploads"
 chmod 600 $ssh_key_path 
 
 # Check if the 'uploads' folder exists on the server/
-ssh -i $ssh_key_path root@$deploy_host "[ -d $uploads_folder ]"
-if [ $? -eq 0 ]; then
-  # If it exists, move it to a temporary location
-  ssh -i $ssh_key_path root@$deploy_host "mv $uploads_folder $deploy_path/uploads_temp"
-fi
+# ssh -i $ssh_key_path root@$deploy_host "[ -d $uploads_folder ]"
+# if [ $? -eq 0 ]; then
+#   # If it exists, move it to a temporary location
+#   ssh -i $ssh_key_path root@$deploy_host "mv $uploads_folder $deploy_path/uploads_temp"
+# fi
 
 # Pull the latest changes from the remote repository (assuming the branch is 'master')
 git fetch origin master
 
 # # Copy only the changed files to the server for backend
-rsync -avz -e "ssh -i $ssh_key_path" backend/ root@$deploy_dir/backend
+rsync -avz --exclude 'uploads/' -e "ssh -i $ssh_key_path" backend/ root@$deploy_dir/backend
+
+# rsync -avz -e "ssh -i $ssh_key_path" backend/ root@$deploy_dir/backend
 
 rsync -avz -e "ssh -i $ssh_key_path"  frontend/build/ root@$deploy_dir
 
 ssh -i $ssh_key_path root@$deploy_host "cd $deploy_path/backend && npm install --legacy-peer-deps && /usr/local/bin/pm2 restart likeme_test"
 
 
-ssh -i $ssh_key_path root@$deploy_host "[ -d $deploy_path/uploads_temp ]"
-if [ $? -eq 0 ]; then
-  # If it was moved, restore it to its original location
-  ssh -i $ssh_key_path root@$deploy_host "mv $deploy_path/uploads_temp $uploads_folder"
-fi
+# ssh -i $ssh_key_path root@$deploy_host "[ -d $deploy_path/uploads_temp ]"
+# if [ $? -eq 0 ]; then
+#   # If it was moved, restore it to its original location
+#   ssh -i $ssh_key_path root@$deploy_host "mv $deploy_path/uploads_temp $uploads_folder"
+# fi
 # ssh -i $ssh_key_path root@$deploy_host "rm -r $deploy_path/backend/build"
 
